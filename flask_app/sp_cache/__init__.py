644453115a8fd0d844843908842395c4c92094f3
CJ Grady <cjgrady@gmail.com>
2021-04-30 11:47:32 -0500

    Combine methods for interacting with a specimen

--- flask_app/sp_cache/__init__.py
+++ flask_app/sp_cache/__init__.py
@@ -55,23 +55,16 @@ def collection_occurrences_modify(collection_id):
 
 
 # .....................................................................................
-@bp.route('/collection/<string:collection_id>/occurrences/<string:identifier>', methods=['DELETE'])
-def collection_occurrence_delete(collection_id, identifier):
-    controller.delete_collection_occurrences(collection_id, [identifier])
-
-
-# .....................................................................................
-@bp.route('/collection/<string:collection_id>/occurrences/<string:identifier>', methods=['GET'])
-def collection_occurrence_get(collection_id, identifier):
-    specimen = controller.get_collection_occurrence(collection_id, identifier)
-    if specimen:
-        return specimen.serialize_json()
-    raise NotFound()
-
-
-# .....................................................................................
-@bp.route('/collection/<string:collection_id>/occurrences/<string:identifier>', methods=['PUT'])
-def collection_occurrence_put(collection_id, identifier):
-    new_specimen_record = SpecimenRecord(request.json)
-    controller.update_collection_occurrences(collection_id, [new_specimen_record])
-    return controller.get_collection_occurrence(collection_id, identifier).serialize_json()
+@bp.route('/collection/<string:collection_id>/occurrences/<string:identifier>', methods=['DELETE', 'GET', 'PUT'])
+def collection_occurrence(collection_id, identifier):
+    if request.method.lower() == 'delete':
+        return collection_occurrence_delete(collection_id, identifier)
+    elif request.method.lower() == 'get':
+        specimen = controller.get_collection_occurrence(collection_id, identifier)
+        if specimen:
+            return specimen.serialize_json()
+        raise NotFound()
+    elif request.method.lower() == 'put':
+        new_specimen_record = SpecimenRecord(request.json)
+        controller.update_collection_occurrences(collection_id, [new_specimen_record])
+        return controller.get_collection_occurrence(collection_id, identifier).serialize_json()
