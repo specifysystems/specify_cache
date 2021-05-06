74571a1eeefbbb852b4f9e24ce7b01148d95f33d
CJ Grady <cjgrady@gmail.com>
2021-05-06 14:16:03 -0500

    Change directory name

--- flask_app/sp_cache/config.py
+++ flask_app/sp_cache/config.py
@@ -7,6 +7,6 @@ SOLR_PORT = 8983
 COLLECTIONS_URL = '{}:{}/solr/sp_collections'.format(SOLR_SERVER, SOLR_PORT)
 SPECIMENS_URL = '{}:{}/solr/specimen_records'.format(SOLR_SERVER, SOLR_PORT)
 
-BACKUP_DATA_PATH = '/state/partition2/syftorium_data/'
+BACKUP_DATA_PATH = '/state/partition1/syftorium_data/'
 COLLECTION_BACKUP_PATH = os.path.join(BACKUP_DATA_PATH, 'collections')
 DWCA_PATH = os.path.join(BACKUP_DATA_PATH, 'new_dwcas')
