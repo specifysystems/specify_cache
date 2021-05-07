60aaf5e4f15121b6dfe4c47ab333deff140185b8
CJ Grady <cjgrady@gmail.com>
2021-05-07 13:08:54 -0500

    Add CORS support

--- flask_app/application.py
+++ flask_app/application.py
@@ -2,6 +2,7 @@
 import os
 
 from flask import Flask
+from flask_cors import CORS
 
 from  flask_app import sp_cache
 
@@ -18,6 +19,7 @@ def create_app(test_config=None):
     """
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    CORS(app)
     app.config.from_mapping(
         SECRET_KEY='dev',
         # DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
