dee88853973fb10a187a802bf16afffefeef80c9
Max Patiiuk <max@patii.uk>
2021-11-16 17:43:24 -0600

    Simplify CRON setup

--- flask_app/sp_cache/manage.py
+++ flask_app/sp_cache/manage.py
@@ -1,8 +1,13 @@
 from flask.cli import FlaskGroup
 
 from flask_app.sp_cache import app
+from flask_app.sp_cache.process_dwca import main as process_dwca
 
 cli = FlaskGroup(app)
+
+@cli.command('process_dwcas')
+def process_dwcas():
+    process_dwca()
 
 if __name__ == '__main__':
     cli()
